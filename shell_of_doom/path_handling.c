9bae304f710e3b94028a8a17f3f0e27a36e3a3ad
ChristianCampos-55 <1566@holbertonschool.com>
2020-04-14 19:00:54 +0000

    adding final shell of doom working as a clock

---
+++ shell_of_doom/path_handling.c
@@ -0,0 +1,113 @@
+#include "library.h"
+
+/**
+ * basic_commands - opens space in memory and allocates directions of
+ * basic commands.
+ * @comando: command to execute.
+ * @head_path: pointer to first element in singly linked list.
+ *
+ * Return: Null.
+ */
+char *basic_commands(char *comando, struct_path *head_path)
+{
+	struct stat st;
+	char *path_cmd = NULL;
+
+	while (head_path->next)
+	{
+		path_cmd = malloc((_strlen(head_path->str) + _strlen(comando)) + 2);
+		_strcat(_strcat(_strcpy(path_cmd, head_path->str), "/"), comando);
+		if (stat(path_cmd, &st) == 0 && st.st_mode & S_IXUSR)
+			return (path_cmd);
+		free(path_cmd);
+		head_path = head_path->next;
+	}
+	return (NULL);
+}
+
+/**
+ * made_the_linked_list_path - creates linked list
+ * made of PATH adresses.
+ * @path: path to compartmentalize.
+ *
+ * Return: head, pointer to first element of linked list.
+ */
+struct_path *made_the_linked_list_path(char *path)
+{
+	struct_path *head_path = NULL;
+	char *token = NULL;
+
+	token = strtok(path, ":");
+	while (token)
+	{
+		add_node_end(&head_path, token);
+		token = strtok(NULL, ":");
+	}
+	return (head_path);
+}
+
+/**
+ * made_the_linked_list_enviroment - creates linked list of enviromental var
+ * @environ: is a double pointer to the enviromental var
+ *
+ * Return: head, pointer to first element of linked list.
+ */
+struct_path *made_the_linked_list_enviroment(char **environ)
+{
+	struct_path *head = NULL;
+	int i;
+
+	for (i = 0; environ[i]; i++)
+/*Se le pasa la direccion de memoria para que pueda modificar head*/
+		add_node_end(&head, environ[i]);
+
+	return (head);
+}
+
+/**
+ * add_node_end - adds as many nodes as needed to store PATH.
+ * @head: first element of linked list.
+ * @str: an adress in path.
+ *
+ * Return: new node.
+ */
+struct_path *add_node_end(struct_path **head, char *str)
+{
+	struct_path *newNode = NULL, *end = *head;
+
+	newNode = malloc(sizeof(struct_path));
+	if (newNode == NULL)
+	{
+		perror("Error: ");
+		return (NULL);
+	}
+	newNode->str = _strdup(str);
+	newNode->next = NULL;
+	if (!*head)
+	{
+		*head = newNode;
+		return (*head);
+	}
+	while (end->next != NULL)
+		end = end->next;
+	end->next = newNode;
+	return (newNode);
+}
+
+/**
+ * print_list - prints list for path.
+ * @h: list to print.
+ *
+ * Return: segments of list to print.
+ */
+int print_list(struct_path *h)
+{
+	int compareer;
+
+	for (compareer = 0; h; compareer++)
+	{
+		printf("%s\n", h->str);
+		h = h->next;
+	}
+	return (compareer);
+}
